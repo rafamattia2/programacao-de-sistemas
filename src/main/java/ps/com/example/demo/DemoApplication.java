6f3c29020bef47b42b55261c113ec59f57d50b42
Gabriel Ramires <falivith@gmail.com>
2023-03-22 01:25:48 -0300

    Reader 80% Completo

--- src/main/java/ps/com/example/demo/DemoApplication.java
+++ src/main/java/ps/com/example/demo/DemoApplication.java
@@ -1,14 +1,31 @@
 package ps.com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Alô");
-	}
+		ConfigurableApplicationContext app = SpringApplication.run(DemoApplication.class, args);
+		String filePath = "src/src.txt";
 
+		Reader reader = new Reader();
+		List<Loaded> program = reader.readObjectCode(filePath);
+
+		for (Loaded codes : program) {
+			String[] binaryLine = codes.toBinary();
+
+			for (String str : binaryLine) {
+				System.out.print(str + " ");
+			}
+			System.out.println("\n");
+		}
+
+		app.close();
+	}
 }
